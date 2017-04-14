24f044b9529687fde638f5ab5cc3e3f6fce871ca
Brian Nash <brianlnash@gmail.com>
2017-04-14 08:53:31 -0700

    Add files via upload

---
+++ css/jquery.js
@@ -0,0 +1,20 @@
+body {
+    font-family: 'Barrio', cursive;
+    color:#747784;
+    font-weight: 300
+}
+.text-muted {
+    color:#BCBBBB;
+}
+
+.text-thick{
+    font-weight: 800
+}
+
+.title-super{
+    font-size: 60px;
+}
+
+.title-logo{
+    margin-top: 20px;
+}
